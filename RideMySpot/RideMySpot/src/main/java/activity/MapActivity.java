e4d644b0d97188e04326030c6abfd8c756e51c27
steeveguillaume <steeve.guillaume@atos.net>
2016-03-11 22:22:06 +0100

    Insert the email intent

--- RideMySpot/RideMySpot/src/main/java/activity/MapActivity.java
+++ RideMySpot/RideMySpot/src/main/java/activity/MapActivity.java
@@ -681,8 +681,12 @@ public class MapActivity extends AppCompatActivity implements LocationListener, 
 				alertDialogDisclaimer();
 				break;
 			case 4 :
-				//email
-				//with the subject and content
+				Intent email = new Intent(Intent.ACTION_SEND);
+				email.putExtra(Intent.EXTRA_EMAIL, new String[]{"youremail@yahoo.com"});
+				email.putExtra(Intent.EXTRA_SUBJECT, "subject");
+				email.putExtra(Intent.EXTRA_TEXT, "message");
+				email.setType("message/rfc822");
+				startActivity(Intent.createChooser(email, "Choose an Email client :"));
 				break;
 		}
 	}
